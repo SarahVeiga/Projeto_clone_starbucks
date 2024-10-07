c6bbaee2f4af3cf14ac0dda8cc72a6fdd7fa21cc
SarahVeiga <sarahveiga55@gmail.com>
2024-10-07 16:30:40 -0300

    Finalização

--- mudar.js
+++ mudar.js
@@ -1,9 +1,20 @@
-// Primeira parte Habilitado/Desabilitado
+// primeira parte Habilitado/Desabilitado
 function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
     var links = document.querySelector('.links');
-    // Troca o estado da classe .toggle para ativo
+    // troca o estado da classe .toggle para ativo
     menuToggle.classList.toggle('active')
-    // Troca o estado da classe .links para ativo
+    // troca o estado da classe .links para ativo 
     links.classList.toggle('active')
+}
+
+// troca o copo grande 
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+// troca a cor do círculo do fundo
+function changeCircleColor(color){
+    const circulo =document.querySelector('.circulo');
+    circulo.style.background = color;
 }
